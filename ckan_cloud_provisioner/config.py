6df9e7dc43a0fc6db55ad1680ddd8d7cf0bf81d5
Adam Kariv <adam.kariv@gmail.com>
2019-03-14 16:03:30 +0200

    Reduce default timeout

--- ckan_cloud_provisioner/config.py
+++ ckan_cloud_provisioner/config.py
@@ -4,7 +4,7 @@ import os
 instance_manager = os.environ.get('INSTANCE_MANAGER')
 
 # Wait for results this long
-default_timeout = 20
+default_timeout = 12
 
 # Cancel a job after this long
 default_cancel_timeout = 300
